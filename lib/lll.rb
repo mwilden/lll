acd8626092496c762fef15ee40becf43f06212ef
Mark Wilden <mark@mwilden.com>
2010-11-26 20:26:06 -0800

    Don't colorize if dumb terminal. Cleanup.

--- lib/lll.rb
+++ lib/lll.rb
@@ -6,33 +6,38 @@
 # Enumerable members are output on separate lines
 
 def lll msg = nil, &block
-  s = " "
-  expression_string = ''
-  expression_value = 0
-  if block_given?
-    s << msg + ': ' if msg
-    expression_string = block.call
-    expression_value = eval(expression_string, block)
-    s << expression_string + ' = '
-    if expression_value.respond_to?(:each) && !expression_value.is_a?(String) && !expression_value.is_a?(Nokogiri::HTML::Document)
-      s << " \n"
-      expression_value.each { |e| s << ' ' << e.inspect << " \n" }
+  Lll.lll msg, block
+end
+
+module Lll
+  def self.lll msg, block = nil
+    output_string = " "
+    expression_value = 0
+    if block
+      output_string << msg + ': ' if msg
+      expression_string = block.call
+      expression_value = eval(expression_string, block)
+      output_string << expression_string + ' = '
+      if expression_value.respond_to?(:each) && !expression_value.is_a?(String) && !expression_value.is_a?(Nokogiri::HTML::Document)
+        output_string << " \n"
+        expression_value.each { |e| output_string << ' ' << e.inspect << " \n" }
+      else
+        output_string << expression_value.inspect << " \n"
+      end
     else
-      s << expression_value.inspect << " \n"
+      output_string << msg if msg
+      output_string << " \n"
     end
-  else
-    s << msg if msg
-    s << " \n"
+
+    Kernel.puts format(output_string, ENV['TERM'] != 'dumb')
+    Rails.logger.debug(format(output_string)) if defined?(Rails) && Rails.logger
+
+    expression_value
   end
 
-  reverse_video_color_code = "\e[7m"
-  reset_color_code = "\e[0m"
-  
-  s = reverse_video_color_code + s + reset_color_code
-  s << 'lll: ' + caller.first.to_s + ' ' + Time.now.strftime('%X')
-
-  Kernel.puts s
-  Rails.logger.debug s if defined?(Rails) && Rails.logger
-
-  expression_value
+  def self.format output_string, colorize = true
+    string = output_string
+    string =  "\e[7m" + string + "\e[0m" if colorize
+    string + "lll: #{caller[2].to_s} #{Time.now.strftime('%X')}"
+  end
 end
